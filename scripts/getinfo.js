acddbf50eb445186c627e547cd6ea7e3abaac033
vaddya <vadik.dyachkov@gmail.com>
2017-02-16 17:08:43 +0300

    fix cols in courses

--- scripts/getinfo.js
+++ scripts/getinfo.js
@@ -5,6 +5,7 @@ $.ajaxSetup({
 });
 $.getJSON('info.json', function (data) {
     var courses = [];
+    const COL_NUM = 2;
     $.each(data.courses, function (index, course) {
         var difficulty = '';
         for (i = 0; i < course.difficulty; i++) {
@@ -13,21 +14,27 @@ $.getJSON('info.json', function (data) {
         for (i = 0; i < 5 - course.difficulty; i++) {
             difficulty += '<span class="glyphicon glyphicon-star-empty"></span>'
         }
-        var html = '<div class="col-sm-6 col-lg-6 col-md-6">' +
+        var html = '';
+        if (index % COL_NUM == 0) {
+            html += '<div class="row row-eq-height">';
+        }
+        html += '<div class="col-sm-6 col-lg-6 col-md-6">' +
             '<div class="thumbnail">' +
-            '<div class="cover"><img src="' + course.logo_url + '" alt="Course logo"></div>' +
+            '<div class="cover"><img src="' + course.logo + '" alt="Course logo"></div>' +
             '<div class="caption">' +
-            '<h4 class="pull-right">' + course.category + '</h4>' +
-            '<h4>' + course.title + '</h4>' +
+            '<h4 class="pull-right"><span class="label label-default">' + course.category + '</span></h4>' +
+            '<h4><span class="glyphicon glyphicon-education" aria-hidden="true"></span> ' + course.title + '</h4>' +
             '<hr>' +
             '<p>' + course.description + '</p>' +
             '<p>' + difficulty + ' Difficulty: ' + course.difficulty + '</p>' +
             '</div>' +
             '</div>' +
-            '</div>' +
             '</div>';
+        if (index % COL_NUM == COL_NUM - 1) {
+            html += '</div>'
+        }
         courses.push(html);
     });
-    $(courses.join("")).appendTo(".row");
+    $(courses.join("")).appendTo("#courses");
     $('<small>Last update: ' + data.update.substr(9) + '</small>').appendTo("#title")
 });
