51aefb5801ecb720fae39661b08a00b58d9f08f5
Anton Lamtev <antonlamtev@gmail.com>
2019-10-18 04:35:00 +0300

    do not store *.pitch in repo. generate them before deploy

--- download_inspector.sh
+++ download_inspector.sh
@@ -10,9 +10,12 @@ download()
   
   TOKEN="$1"
   
-  curl -H "Authorization: token $TOKEN" https://api.github.com/repos/lezhenin/prof-higgins/releases/latest > latest.json
-  ID=`cat latest.json | jq '.assets[] | select(.name=="inspector.zip") | .id'`
+  curl -H "Authorization: token $TOKEN" https://api.github.com/repos/StudyIntonation/course-inspector/releases/latest > latest.json
+  INSPECTOR_ID=`cat latest.json | jq '.assets[] | select(.name=="inspector.zip") | .id'`
+  PITCH_DETECTOR_ID=`cat latest.json | jq '.assets[] | select(.name=="pitch_detector") | .id'`
   rm latest.json
-  wget --auth-no-challenge --header='Accept:application/octet-stream' https://${TOKEN}:@api.github.com/repos/lezhenin/prof-higgins/releases/assets/${ID} -O inspector.zip
+  wget --auth-no-challenge --header='Accept:application/octet-stream' https://${TOKEN}:@api.github.com/repos/StudyIntonation/course-inspector/releases/assets/${INSPECTOR_ID} -O inspector.zip
+  wget --auth-no-challenge --header='Accept:application/octet-stream' https://${TOKEN}:@api.github.com/repos/StudyIntonation/course-inspector/releases/assets/${PITCH_DETECTOR_ID} -O pitch_detector
   
 } &> /dev/null
+
